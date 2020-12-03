749d20757a4e57f464cddc72bc2ba35b210c9ff7
rivit98 <albert98g@gmail.com>
2020-12-03 14:43:35 +0100

    dirwatcher improved version

--- DiskStalker/src/main/java/filesystem/DirWatcher.java
+++ DiskStalker/src/main/java/filesystem/DirWatcher.java
@@ -1,17 +1,20 @@
 package filesystem;
 
-import io.reactivex.rxjava3.core.ObservableEmitter;
-import model.FileData;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.subjects.PublishSubject;
 
 import java.io.IOException;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class DirWatcher {
     private final WatchService watchService;
     private boolean shutdown = false;
     private final Thread monitor;
+    private final PublishSubject<WatchKey> subject = PublishSubject.create();
 
     public DirWatcher(WatchService watchService) {
         this.watchService = watchService;
@@ -23,19 +26,14 @@ public class DirWatcher {
         monitor.interrupt();
     }
 
-    public void startMonitoring(){
-        System.out.println("monitoring started");
+    public Observable<WatchKey> watchForChanges(){
         monitor.start();
+        return subject;
     }
 
     private void emitKey(WatchKey key){
         //TODO: register newly created dirs
-        System.out.println(key);
-        for (final WatchEvent<?> event : key.pollEvents()) {
-            System.out.println(event.kind().name() + " " + event.context());
-        }
-
-        key.reset();
+        subject.onNext(key);
     }
 
     private WatchKey takeKey(){
@@ -52,13 +50,15 @@ public class DirWatcher {
             if(watchKey != null){
                 emitKey(watchKey);
             }else{ //thread was interrupted, finish work
-                System.out.println("close thread");
-                try{
-                    watchService.close();
-                } catch (IOException exception) {
-                    //we dont care
+                if(shutdown){
+                    System.out.println("close thread");
+                    try{
+                        watchService.close();
+                    } catch (IOException exception) {
+                        //we dont care
+                    }
+                    break;
                 }
-                break;
             }
         }
     }
--- DiskStalker/src/main/java/model/ObservableFolder.java
+++ DiskStalker/src/main/java/model/ObservableFolder.java
@@ -2,7 +2,6 @@ package model;
 
 import filesystem.DirWatcher;
 import filesystem.FileTreeScanner;
-import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -10,67 +9,95 @@ import javafx.collections.ObservableList;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.HashMap;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
+
+// TODO: CLOSE WATCHSERVICE (OBSERVABLE FOLDER) WHEN DELETING ITEM FROM TREEVIEW OR ****CLOSING APP****
 
 public class ObservableFolder {
-    private final ObservableList<File> files = FXCollections.observableArrayList();
     private final ObservableList<WatchKey> keys = FXCollections.observableArrayList();
     private final HashMap<WatchKey, File> directoryMap = new HashMap<>(); //TODO: remove proper key after deleting node
-    private final WatchService watchService; //TODO: remember to close this after deleting node
+    private WatchService watchService; //TODO: remember to close this after deleting node
     //TODO: map File - TreeItem
-    private final Path directoryPath;
-    private final TreeBuilder treeBuilder;
-    private final DirWatcher watcher;
+    private final Path dirToWatch;
+    private TreeBuilder treeBuilder;
+    private DirWatcher dirWatcher;
 
     public ObservableFolder(Path dirToWatch) throws IOException {
-        directoryPath = dirToWatch;
-        watchService = dirToWatch.getFileSystem().newWatchService();
-        treeBuilder = new TreeBuilder(dirToWatch);
-        watcher = new DirWatcher(watchService);
+        this.dirToWatch = dirToWatch;
+        this.watchService = dirToWatch.getFileSystem().newWatchService();
+        this.treeBuilder = new TreeBuilder(dirToWatch);
+        this.dirWatcher = new DirWatcher(watchService);
 
-        scanDirectory();
+        scan();
     }
 
     public void scanDirectory() {
         var scanner = new FileTreeScanner(watchService);
         scanner
-                .scanDirectory(directoryPath)
+                .scanDirectory(dirToWatch)
 //                .zipWith(Observable.interval(300, TimeUnit.MILLISECONDS), (item, notUsed) -> item)
                 .subscribeOn(Schedulers.io())
-                .subscribe(
-                        (FileData fd) -> {
-                            directoryMap.put(fd.getEvent(), fd.getFile());
-                            treeBuilder.addItem(fd);
-//                            files.add(f);
-                            keys.add(fd.getEvent());
-//                            System.out.println(f);
-                        },
-                        (Throwable e) -> System.out.println(e),
-                        () -> {
-                            // TODO: implement watching system
-                            // TODO: add new watch item for newly created directories
-                            System.out.println("Scanning finished");
-                            for(var k : keys.filtered(Objects::nonNull)){
-                                System.out.println(k);
-                            }
-                            watcher.startMonitoring();
-                        }
+                .subscribe(this::processFileData,
+                        System.out::println,
+                        this::startMonitoring
                 );
     }
 
-    public TreeFileNode getTree() {
-        return treeBuilder.getRoot();
+    public void processFileData(FileData fileData) {
+        directoryMap.put(fileData.getEvent(), fileData.getFile());
+        treeBuilder.addItem(fileData);
+        keys.add(fileData.getEvent());
+        System.out.println(fileData.getFile().getName() + " " +  fileData.getEvent());
     }
 
-    public void closeFolder(){
+    public void startMonitoring(){
+        // TODO: implement watching system
+        // TODO: add new watch key for newly created directories
+        System.out.println("Start monitoring");
+        dirWatcher.watchForChanges().subscribe(this::processKey);
+    }
+
+    public void processKey(WatchKey key){
+        System.out.println(key);
+        for (final WatchEvent<?> event : key.pollEvents()) {
+            System.out.println(event.kind().name() + " " + event.context() + " count " + event.count());
+        }
+
+        var valid = key.reset();
+        //TODO: if not valid, remove treeitem, remove watchkey from map
+        System.out.println(valid);
+
+        //TODO: removing could be done as getParent.remove(me)
+    }
+
+    public void refresh() throws IOException { //TODO: test this!
+        cleanup();
+        scan();
+    }
+
+    public void scan() throws IOException {
+        watchService = dirToWatch.getFileSystem().newWatchService();
+        treeBuilder = new TreeBuilder(dirToWatch);
+        dirWatcher = new DirWatcher(watchService);
+
+        scanDirectory();
+    }
+
+    public void cleanup() {
         try {
+            dirWatcher.stop();
             watchService.close();
+            keys.clear();
+            directoryMap.clear();
         } catch (IOException exception) {
             exception.printStackTrace(); //TODO: what should we do here? :/
         }
+    }
+
+    public TreeFileNode getTree() {
+        return treeBuilder.getRoot(); //TODO: after refreshing tree we have to notify main view about change, maybe binding?
     }
 }
